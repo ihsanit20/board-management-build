f023d2591eaf3432a6c617e3cf1f7ff6bf0d9eb7
Md. Mahdi Hasan <96673899+mahdihasan28@users.noreply.github.com>
2024-10-11 01:17:06 +0600

    print application

---
+++ public/assets/NoticePublicShow-uakgIyDd.js
@@ -0,0 +1,1 @@
+import e from"./NoticeShow-BCLmcmJH.js";import{c as o,d as c,o as t}from"./index-BLqg7Ylw.js";const a={class:"my-container my-10"},i={__name:"NoticePublicShow",setup(r){return(s,_)=>(t(),o("div",a,[c(e)]))}};export{i as default};
