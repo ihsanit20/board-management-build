458fb627a5611c240fb8975858dd5ce92bc595d0
mahdihasan28 <mahdihasan28@gmail.com>
2024-10-20 22:14:11 +0600

    bug solve

---
+++ public/assets/NoticePublicShow-Rwwlt5zk.js
@@ -0,0 +1,1 @@
+import e from"./NoticeShow-DuAxcCyb.js";import{c as o,d as c,o as t}from"./index-B2JbgT2f.js";const a={class:"my-container my-10"},i={__name:"NoticePublicShow",setup(r){return(s,_)=>(t(),o("div",a,[c(e)]))}};export{i as default};
