85ab8cb4df5192b367b79c371a87f4d74cac1300
mahdihasan28 <mahdihasan28@gmail.com>
2024-10-23 23:49:02 +0600

    huge edit

---
+++ public/assets/NoticePublicShow-PA5bKsuF.js
@@ -0,0 +1,1 @@
+import e from"./NoticeShow-BEsXdZAa.js";import{c as o,d as c,o as t}from"./index-BYu76FO-.js";const a={class:"my-container my-10"},i={__name:"NoticePublicShow",setup(r){return(s,_)=>(t(),o("div",a,[c(e)]))}};export{i as default};
