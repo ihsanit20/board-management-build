a242abb934f132bdbe46c97a84cbcf3dd6d71d5a
mahdihasan28 <mahdihasan28@gmail.com>
2024-10-28 22:29:32 +0600

    list print

---
+++ public/assets/NoticePublicShow-ftfQRpp5.js
@@ -0,0 +1,1 @@
+import e from"./NoticeShow-DpKPqXom.js";import{c as o,d as c,o as t}from"./index-BTuRP1DP.js";const a={class:"my-container my-10"},i={__name:"NoticePublicShow",setup(r){return(s,_)=>(t(),o("div",a,[c(e)]))}};export{i as default};
