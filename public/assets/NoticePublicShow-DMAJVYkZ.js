4909f185aab4940a300476240f19332ac46729f3
mahdihasan28 <mahdihasan28@gmail.com>
2024-10-29 12:09:47 +0600

    গগগগগ

---
+++ public/assets/NoticePublicShow-DMAJVYkZ.js
@@ -0,0 +1,1 @@
+import e from"./NoticeShow-CxcuDven.js";import{c as o,d as c,o as t}from"./index-B2GPrU01.js";const a={class:"my-container my-10"},i={__name:"NoticePublicShow",setup(r){return(s,_)=>(t(),o("div",a,[c(e)]))}};export{i as default};
