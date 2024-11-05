98b14c20af0bb60a2016ea6846fc88917a136639
mahdihasan28 <mahdihasan28@gmail.com>
2024-11-05 23:54:11 +0600

    edit

---
+++ public/assets/NoticePublicShow-DKkUEpfy.js
@@ -0,0 +1,1 @@
+import e from"./NoticeShow-BFZA1cHX.js";import{c,f as o,d as t}from"./index-Dml9BmmA.js";const a={class:"my-container my-10"},i={__name:"NoticePublicShow",setup(r){return(s,_)=>(t(),c("div",a,[o(e)]))}};export{i as default};
