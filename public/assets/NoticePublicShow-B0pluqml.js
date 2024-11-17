5c65f11571ead8c47b2499b33b549cd01f6532cc
mahdihasan28 <mahdihasan28@gmail.com>
2024-11-17 11:27:01 +0600

    সভাপতি

---
+++ public/assets/NoticePublicShow-B0pluqml.js
@@ -0,0 +1,1 @@
+import e from"./NoticeShow-DbLtlhvZ.js";import{c,f as o,d as t}from"./index-DyYs2ZW8.js";const a={class:"my-container my-10"},i={__name:"NoticePublicShow",setup(r){return(s,_)=>(t(),c("div",a,[o(e)]))}};export{i as default};
