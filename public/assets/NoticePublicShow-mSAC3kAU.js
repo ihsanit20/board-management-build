ceb094f2501be7466fe529fe9a505146f8854d53
mahdihasan28 <mahdihasan28@gmail.com>
2024-12-03 15:11:50 +0600

    buttons

---
+++ public/assets/NoticePublicShow-mSAC3kAU.js
@@ -0,0 +1,1 @@
+import e from"./NoticeShow-D6gXOOki.js";import{c,f as o,d as t}from"./index-BnHJlrLL.js";const a={class:"my-container my-10"},i={__name:"NoticePublicShow",setup(r){return(s,_)=>(t(),c("div",a,[o(e)]))}};export{i as default};
