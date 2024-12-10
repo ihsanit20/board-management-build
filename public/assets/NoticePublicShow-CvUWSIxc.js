41dce3225348a6760701092fb14a64d2b9f9fad5
mahdihasan28 <mahdihasan28@gmail.com>
2024-12-10 20:40:54 +0600

    admit done

---
+++ public/assets/NoticePublicShow-CvUWSIxc.js
@@ -0,0 +1,1 @@
+import e from"./NoticeShow-BbPV2bJA.js";import{c,f as o,d as t}from"./index-Cg6gaFga.js";const a={class:"my-container my-10"},i={__name:"NoticePublicShow",setup(r){return(s,_)=>(t(),c("div",a,[o(e)]))}};export{i as default};
