f1e25c146b0d965ff76b3def5f80b814640c1bb0
mahdihasan28 <mahdihasan28@gmail.com>
2025-01-03 20:34:49 +0600

    exam subjects ass

---
+++ public/assets/NoticePublicShow-B-An_1hF.js
@@ -0,0 +1,1 @@
+import e from"./NoticeShow-B_U_r6Kb.js";import{c,f as o,d as t}from"./index-CuVS5FRI.js";const a={class:"my-container my-10"},i={__name:"NoticePublicShow",setup(r){return(s,_)=>(t(),c("div",a,[o(e)]))}};export{i as default};
