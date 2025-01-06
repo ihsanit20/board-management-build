24afc2060afa140121bde6b34d84b7f480e80653
Md. Mahdi Hasan <96673899+mahdihasan28@users.noreply.github.com>
2025-01-06 17:18:48 +0600

    25

---
+++ public/assets/NoticePublicShow-CQkbeVb4.js
@@ -0,0 +1,1 @@
+import e from"./NoticeShow-gVKBtUpc.js";import{c,f as o,d as t}from"./index-CCv8xFqe.js";const a={class:"my-container my-10"},i={__name:"NoticePublicShow",setup(r){return(s,_)=>(t(),c("div",a,[o(e)]))}};export{i as default};
