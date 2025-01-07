40c4e9b4323bbab96b84c2754e8a55d0e3e7d33e
mahdihasan28 <mahdihasan28@gmail.com>
2025-01-07 11:07:21 +0600

    mufti

---
+++ public/assets/NoticePublicShow-BuBpAGiX.js
@@ -0,0 +1,1 @@
+import e from"./NoticeShow-CZrY93_S.js";import{c,f as o,d as t}from"./index-ClzD5fxD.js";const a={class:"my-container my-10"},i={__name:"NoticePublicShow",setup(r){return(s,_)=>(t(),c("div",a,[o(e)]))}};export{i as default};
