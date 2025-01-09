c6e7f4fa5557d0b0bc82a3e5ab601fadb1fda15f
mahdihasan28 <mahdihasan28@gmail.com>
2025-01-10 02:39:31 +0600

    1111

---
+++ public/assets/NoticePublicShow-hTKVzjMp.js
@@ -0,0 +1,1 @@
+import e from"./NoticeShow-d4gQLPBh.js";import{c,f as o,d as t}from"./index-D4Zoog1v.js";const a={class:"my-container my-10"},i={__name:"NoticePublicShow",setup(r){return(s,_)=>(t(),c("div",a,[o(e)]))}};export{i as default};
